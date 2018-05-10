75435c3873ff540b1ef5971eab1f16b6d8366dde
Duncan <loxa10077@gmail.com>
2018-05-10 17:04:28 +0800

    create item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_10_085002) do
+ActiveRecord::Schema.define(version: 2018_05_10_085810) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.string "date"
+    t.string "location"
+    t.text "intro"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
