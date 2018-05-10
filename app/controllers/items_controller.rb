a0dd227a1b01ee0a08085fe325a9fa8390d43810
Duncan <loxa10077@gmail.com>
2018-05-10 22:02:03 +0800

    add items Edit&Destroy action and Show Edit page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -13,14 +14,43 @@ class ItemsController < ApplicationController
       flash[:notice] = '項目建立成功'
       redirect_to items_path
     else
-      flash[:alert] = '項目建議有誤'
+      flash[:alert] = '項目建立有誤'
       render :new
     end
   end
+
+  def show
+  end
+
+  def edit  
+  end
+
+  def update
+    
+    if @item.update(item_params)
+      flash[:notice] = '項目更新成功'
+      redirect_to items_path
+    else
+      flash[:alert] = '項目更新有誤'
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    flash[:notice] = '項目已經刪除'
+    redirect_to items_path
+  end
+
 
   private
 
   def item_params
     params.require(:item).permit(:title, :date, :location, :intro)
   end
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
 end
