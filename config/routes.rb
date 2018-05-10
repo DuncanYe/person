c6cf7cc1c8022091ffa111d1cc4172b1f9354d11
Duncan <loxa10077@gmail.com>
2018-05-10 17:48:58 +0800

    add two Routes for log in and log out

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :items
+  authenticated :user do 
+    root "items#index", as: "authenticated_root"
+  end 
   get 'welcomes/about_me'
   root "welcomes#about_me"
 end
