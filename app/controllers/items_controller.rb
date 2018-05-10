53d7dc3311c54613e389da11fff2cfaddaed7185
Duncan <loxa10077@gmail.com>
2018-05-10 20:13:16 +0800

    add items create action and index%new page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,24 @@
 class ItemsController < ApplicationController
   def index
-    
+    @items = Item.order(created_at: :desc)
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :date, :location, :intro)
   end
 end
