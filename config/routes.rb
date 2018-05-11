27aece424f75d351a4133821d364da11971f10a1
Duncan <loxa10077@gmail.com>
2018-05-11 20:58:26 +0800

    set up comment Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :items
+  resources :items do
+    resources :comments, only: [:create, :destroy]
+  end
   resources :genres, only: :show
   authenticated :user do 
     root "items#index", as: "authenticated_root"
