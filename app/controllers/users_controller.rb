f8d39c2dd2ce2e2b54af712d83e1ec5823545303
Duncan <loxa10077@gmail.com>
2018-05-11 19:27:47 +0800

    add user edit page and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,24 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [ :show :edit, :update ]
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(params.require(:user).permit(:name, :intro))
+      flash[:notice] = "個人資料更新成功"
+      redirect_to @user
+    else
+      flash[:alert] = "個人資料更新有誤"
+    end
+  end
+
+  private
+
+  def find_user
     @user = User.find(params[:id])
   end
 end
