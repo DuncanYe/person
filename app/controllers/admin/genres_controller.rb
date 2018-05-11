7e2e51e915d202fc71804bcb1b26485c1626ef53
Duncan <loxa10077@gmail.com>
2018-05-11 10:21:24 +0800

    create Genre create&update&destroy action and display Genre index page

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,51 @@
 class Admin::GenresController < ApplicationController
   before_action :authenticate_admin
+  before_action :find_genre, only: [:update, :destroy ]
   def index
-  
+    @genres = Genre.order(created_at: :desc)
+    if params[:id]
+      @genre = Genre.find(params[:id])
+    else
+      @genre = Genre.new
+    end
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      flash[:notice] = '成功建立活動類型' 
+      redirect_to admin_genres_path
+    else
+      @genres = Genre.order(created_at: :desc)
+      render :index
+    end
+  end
+
+  def update
+    if @genre.update(genre_params)
+      flash[:notice] = '成功更新活動類型' 
+      redirect_to admin_genres_path
+    else
+      @genres = Genre.order(created_at: :desc)
+      render :index
+    end
+  end
+
+  def destroy
+    @genre.destroy
+    flash[:notice] = '活動類型已刪除'
+    redirect_to admin_genres_path
+  end
+
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
+  def find_genre
+    @genre = Genre.find(params[:id])
   end
 
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,6 @@
 class Genre < ApplicationRecord
   has_many :items
   # 如想要刪除該Genre時，底下的items也一並刪除，加入dependent: :destroy， 
+
+  validates_presence_of :name
 end
