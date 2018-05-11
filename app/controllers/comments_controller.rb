218662f21e93241f4c8e65cb914133278950e93e
Duncan <loxa10077@gmail.com>
2018-05-11 23:23:34 +0800

    create comment action and display on items show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+  def create
+    @item = Item.find(params[:item_id])
+    @comment = @item.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save!
+      flash[:notice] = "留言成功"
+      redirect_to item_path(@item)
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
