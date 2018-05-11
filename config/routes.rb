36c769770611a66ae2bbda378f9e287076cd1104
Duncan <loxa10077@gmail.com>
2018-05-11 12:14:10 +0800

    setup public genre reoute and create public geenre controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :items
+  resources :genres, only: :show
   authenticated :user do 
     root "items#index", as: "authenticated_root"
   end 
