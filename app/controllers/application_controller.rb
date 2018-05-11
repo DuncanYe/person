3c4c81c034c52cea8526fe2bab18fd9cc1496630
Duncan <loxa10077@gmail.com>
2018-05-11 08:09:43 +0800

    add admin authenticate method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  def authenticate_admin
+    unless current_user.admin?
+      flash[:alert] = '網頁尚未建立成功'
+      redirect_to root_path
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_10_085810) do
+ActiveRecord::Schema.define(version: 2018_05_11_000343) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2018_05_10_085810) do
     t.text "intro"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
