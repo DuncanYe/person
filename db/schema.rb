6536c5d4f5dfbd821fab16687c1d93746dee5b13
Duncan <loxa10077@gmail.com>
2018-07-03 09:45:00 +0800

    cdd followship create action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_03_010019) do
+ActiveRecord::Schema.define(version: 2018_07_03_013723) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -27,8 +27,6 @@ ActiveRecord::Schema.define(version: 2018_07_03_010019) do
     t.integer "following_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["following_id"], name: "index_followships_on_following_id"
-    t.index ["user_id"], name: "index_followships_on_user_id"
   end
 
   create_table "genres", force: :cascade do |t|
