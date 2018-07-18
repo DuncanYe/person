75119ecf74d11b75eae31348237bd3af28a2724f
Duncan <loxa10077@gmail.com>
2018-07-18 15:50:51 +0800

    g controller admin::items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_11_024905) do
+ActiveRecord::Schema.define(version: 2018_07_18_074230) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -55,6 +55,15 @@ ActiveRecord::Schema.define(version: 2018_07_11_024905) do
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_likes_on_item_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
