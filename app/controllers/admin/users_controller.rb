4b7fec3d5e480322eaa8ffa732a369259653e3da
Duncan <loxa10077@gmail.com>
2018-07-19 19:02:17 +0800

    admin can seach on admin users index using ransack

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,10 @@
 class Admin::UsersController < ApplicationController
 
   def index
-    @users = User.order(created_at: :desc)
+    # @users = User.order(created_at: :desc)
+
+    @ransack = User.ransack(params[:q])
+    @users =  @ransack.result(distinct: true)
   end
 
 end
