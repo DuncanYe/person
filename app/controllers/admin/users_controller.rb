ffda8bbf18f10c03b5a5b0c2511bb49e64808e0c
Duncan <loxa10077@gmail.com>
2018-07-19 20:58:08 +0800

    add strong_parameter to admin users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,17 @@
 class Admin::UsersController < ApplicationController
+  ALLOW_QUERIES = %w[s id_eq name_cont likes_count_gteq, role_cont ].freeze
 
   def index
     # @users = User.order(created_at: :desc)
 
-    @ransack = User.ransack(params[:q])
+    @ransack = User.ransack(ransack_params)
     @users =  @ransack.result(distinct: true)
+  end
+
+  private
+  
+  def ransack_params
+    params.require(:q).permit(*ALLOW_QUERIES) if params[:q].present?
   end
 
 end
