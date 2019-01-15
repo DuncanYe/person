1adbfaf61c5a50681f737817461bd9f98eaf457b
Duncan <loxa10077@gmail.com>
2019-01-15 20:29:11 +0800

    set up AWS SES account

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,21 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
+   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+       address:              'email-smtp.us-east-1.amazonaws.com',
+       port:                 587,
+       user_name:            '',
+       password:             '',
+       authentication:       'login',
+       enable_starttls_auto: true,
+       return_response: true
+    }
+
+
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
