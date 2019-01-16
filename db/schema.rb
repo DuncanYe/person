0a73e91aa50bc63e044f6890b3155a383bed3870
Duncan <loxa10077@gmail.com>
2019-01-16 12:45:15 +0800

    add email confirm column to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_07_224314) do
+ActiveRecord::Schema.define(version: 2019_01_16_022428) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -113,6 +113,8 @@ ActiveRecord::Schema.define(version: 2018_10_07_224314) do
     t.integer "registers_count", default: 0
     t.string "fb_uid"
     t.string "fb_token"
+    t.boolean "email_confirmed", default: false
+    t.string "confirm_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
